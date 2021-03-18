7a368e4a0c34840715b10eb8da32781c95680bc0
beningitis <benjamon0118@gmail.com>
2021-03-17 22:32:52 -0500

    Created class to implement the food object

---
+++ Snek/food.py
@@ -0,0 +1,30 @@
+import pygame
+import random
+from Snek.constants import (
+    SCREEN_WIDTH,
+    SCREEN_HEIGHT,
+    BLOCK_WIDTH,
+    RED,
+    GREEN,
+    BLUE,
+    WHITE,
+    BLACK
+)
+
+
+class Food(pygame.sprite.Sprite):
+    # Represents the block of food
+
+    def __init__(self):
+        # Constructor.  Creates the food block that the snake eats
+        super().__init__()
+        self.surf = pygame.Surface((BLOCK_WIDTH, BLOCK_WIDTH))
+        self.surf.fill(RED)
+        self.rect = self.surf.get_rect()
+        self.update()
+
+    def update(self):
+        # Called when the food is eaten
+        self.rect.y = random.randrange(0, SCREEN_HEIGHT - BLOCK_WIDTH, BLOCK_WIDTH)
+        self.rect.x = random.randrange(0, SCREEN_WIDTH - BLOCK_WIDTH, BLOCK_WIDTH)
+
