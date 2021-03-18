dfc4756a602c9377cd6293e0932525839f72bbcb
beningitis <benjamon0118@gmail.com>
2021-03-17 22:34:01 -0500

    Class to implement game instance

--- Snek/snake.py
+++ Snek/snake.py
@@ -1,5 +1,4 @@
 import pygame
-
 from pygame.locals import (
     K_UP,
     K_DOWN,
@@ -10,20 +9,41 @@ from pygame.locals import (
     QUIT,
 )
 
-# Screen width and height
-SCREEN_WIDTH = 600
-SCREEN_HEIGHT = 600
+from Snek.constants import (
+    SCREEN_WIDTH,
+    SCREEN_HEIGHT,
+    BLOCK_WIDTH,
+    RED,
+    GREEN,
+    BLUE,
+    WHITE,
+    BLACK
+)
 
 
 class Snake(pygame.sprite.Sprite):
+    # Represents the snake
+
     def __init__(self):
-        super(Snake, self).__init__()
-        self.x_velocity = 0;
-        self.y_velocity = 0;
-        self.surf = pygame.Surface((24, 24))
+        super().__init__()
+        # Snek does not move initially
+        self.x_velocity = 0
+        self.y_velocity = 0
+
+        self.x = SCREEN_WIDTH / 2
+        self.y = SCREEN_HEIGHT / 2
+
+        self.position = [self.x, self.y]
+
+        # surf is a 24x24 px surface, filled with white
+        self.surf = pygame.Surface((BLOCK_WIDTH, BLOCK_WIDTH))
         self.surf.fill('#ffffff')
+
         self.rect = self.surf.get_rect()
-        self.trail = []
+        # Center of rect object is at position[]
+        self.rect.centerx = self.position[0]
+        self.rect.centery = self.position[1]
+
         # loads the high score data from file
         # self.load_data()
 
@@ -44,9 +64,11 @@ class Snake(pygame.sprite.Sprite):
             self.y_velocity = 0
         if self.rect.left < 0:
             self.rect.left = 0
+        # Make sure snake stays within window
         if self.rect.right > SCREEN_WIDTH:
             self.rect.right = SCREEN_WIDTH
         if self.rect.top <= 0:
             self.rect.top = 0
         if self.rect.bottom >= SCREEN_HEIGHT:
             self.rect.bottom = SCREEN_HEIGHT
+
